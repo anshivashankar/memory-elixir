da1bddef65b952fe007aaa9df218e8e4e9b75393
Shivvy <shivashankar.a@husky.neu.edu>
2018-10-01 23:24:50 -0400

    Added BackupServer

--- lib/memory_web/channels/games_channel.ex
+++ lib/memory_web/channels/games_channel.ex
@@ -1,14 +1,18 @@
 defmodule MemoryWeb.GamesChannel do
   use MemoryWeb, :channel
 
+  alias Memory.BackupAgent
+
+
   # join and handle_in methods inspired by: http://www.ccs.neu.edu/home/ntuck/courses/2018/09/cs4550/notes/06-channels/games_channel.ex
-  # as well as 
+  # as well as  http://www.ccs.neu.edu/home/ntuck/courses/2018/09/cs4550/notes/07-state/notes.html.
   def join("games:" <> name, payload, socket) do
     if authorized?(payload) do
-      game = Memory.new()
+      game = BackupAgent.get(name) || Memory.new()
       socket = socket
       |> assign(:game, game)
       |> assign(:name, name)
+      BackupAgent.put(name, game)
       {:ok, %{"join"=> name, "game" => Memory.client_view(game)}, socket}
     else
       {:error, %{reason: "unauthorized"}}
@@ -25,22 +29,28 @@ defmodule MemoryWeb.GamesChannel do
   # broadcast to everyone in the current topic (games:lobby).
   # payload should always be a number, the id of the card picked by the client.
   def handle_in("guess_card", payload, socket) do
+    name = socket.assigns[:name]
     game = Memory.guess_card(socket.assigns[:game], payload)
     socket = assign(socket, :game, game)
+    BackupAgent.put(name, game)
     {:reply, {:ok, %{ "game" => Memory.client_view(game)}}, socket}
     #broadcast socket, "shout", payload
     #{:noreply, socket}
   end
 
   def handle_in("end_guess", _, socket) do
+    name = socket.assigns[:name]
     game = Memory.end_guess(socket.assigns[:game])
     socket = assign(socket, :game, game)
+    BackupAgent.put(name, game)
     {:reply, {:ok, %{ "game" => Memory.client_view(game)}}, socket}
   end
 
   def handle_in("reset_game", _, socket) do
+    name = socket.assigns[:name]
     game = Memory.reset_game()
     socket = assign(socket, :game, game)
+    BackupAgent.put(name, game)
     {:reply, {:ok, %{ "game" => Memory.client_view(game)}}, socket}
   end
 
